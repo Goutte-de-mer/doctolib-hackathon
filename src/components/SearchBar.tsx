a87be4e415e4eebdb5d2684f7138f16e97ec1b3a
o.gazzah <gazzahomar2001@gmail.com>
2025-09-25 14:39:09 +0200

    add omar

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,19 @@
+export default  function SearchBar () {
+
+    return(
+        <div className=" flex bg-white rounded-2xl p-7 w-full gap-4">
+
+            <img src="images/icons/iconamoon_search.png" className=" w-5"/> 
+
+            <input type="text"  className=" flex-1 blue-text" placeholder="Cherchez un médecin, un soin, un établissement de santé ..."/>
+
+
+            
+
+
+
+
+            
+        </div>
+    )
+}
