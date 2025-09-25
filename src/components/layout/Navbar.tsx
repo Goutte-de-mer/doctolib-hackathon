c49f7109cbbeb5a958df5b18cb1399961d73390e
Marion <marionbailleux07@gmail.com>
2025-09-25 23:00:04 +0200

    fff

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -6,8 +6,13 @@ import Link from "next/link";
 const Navbar = () => {
   const pathname = usePathname();
   return (
-    <nav className="bg-white py-3 px-0.5 rounded-xl grid grid-cols-5 fixed bottom-2 shadow-[0_-4px_6px_-1px_rgba(0,0,0,0.1)] left-1/2 transform -translate-x-1/2 w-[95%]">
-      <Link href={"/"} className="flex flex-col items-center">
+    <nav className="bg-white py-3 px-0.5 rounded-xl grid grid-cols-5 fixed bottom-2 shadow-[0_-4px_6px_-1px_rgba(0,0,0,0.1)] left-1/2 -translate-x-1/2 w-[95%]">
+      <Link
+        href={"/"}
+        className={`flex flex-col items-center relative ${
+          pathname === "/" ? "after" : ""
+        }`}
+      >
         <Image
           src={`${pathname === "/" ? "/house-yellow.png" : "/house.png"}`}
           alt=""
@@ -17,7 +22,12 @@ const Navbar = () => {
         <p className="navText">Accueil</p>
       </Link>
 
-      <Link href={"/appointments"} className="flex flex-col items-center">
+      <Link
+        href={"/appointments"}
+        className={`flex flex-col items-center relative ${
+          pathname === "/appointments" ? "after" : ""
+        }`}
+      >
         <Image
           src={`${
             pathname === "/appointments"
@@ -30,15 +40,15 @@ const Navbar = () => {
         />
         <p className="navText">Rendez-vous</p>
       </Link>
-      <Link href={"/"} className="flex flex-col items-center">
+      <Link href={"/"} className={`flex flex-col items-center`}>
         <Image src={`/heart.png`} alt="" width={25} height={25} />
         <p className="navText">Santé</p>
       </Link>
-      <Link href={"/"} className="flex flex-col items-center">
+      <Link href={"/"} className={`flex flex-col items-center`}>
         <Image src={`/envelope.png`} alt="" width={25} height={25} />
         <p className="navText">Messages</p>
       </Link>
-      <Link href={"/"} className="flex flex-col items-center">
+      <Link href={"/"} className={`flex flex-col items-center`}>
         <Image src={`/user-gear.png`} alt="" width={25} height={25} />
         <p className="navText">Compte</p>
       </Link>
